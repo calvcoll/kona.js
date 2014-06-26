2a79d19e3650a72be99bb46758ac04dcd168a805
Calvin Collins <calvcoll@gmail.com>
2014-06-26 21:48:05 +0100

    Added in ability for timed downloading.

--- reqkona.js
+++ reqkona.js
@@ -32,31 +32,35 @@ var opts = require('nomnom')
 	})
 	.parse();
 
-hosts = ["http://konachan.com","http://yande.re"];
-
-host = hosts[Math.floor(Math.random() * 2)];
-
 sfw = opts.sfw
 if (opts.debug) console.log("sfw?: " + sfw)
 time = opts.time
 if (opts.debug) console.log("time: " + time)
 
+hosts = ["http://konachan.com","http://yande.re"];
 
-console.log("Host: " + host)
+var download = function() {
+	host = hosts[Math.floor(Math.random() * 2)];
 
-request(host + "/post.json?limit=1", function(error, response, body){
+	console.log("Host: " + host)
 
-	if (!error && response.statusCode == 200) {
-		json = JSON.parse(body)[0]
-		if (opts.debug) console.log(json)
-		file_url = json.file_url
-		if (opts.debug) console.log(file_url)
-		if (file_url != undefined) {
-			console.log("Downloading " + file_url)
-			var file_name = querystring.unescape(file_url.split('/')[file_url.split('/').length - 1])
-			console.log("File name is :" + file_name)
-			request(file_url).pipe(fs.createWriteStream(file_name))
+	request(host + "/post.json?limit=1", function(error, response, body){
+
+		if (!error && response.statusCode == 200) {
+			json = JSON.parse(body)[0]
+			if (opts.debug) console.log(json)
+			file_url = json.file_url
+			if (opts.debug) console.log(file_url)
+			if (file_url != undefined) {
+				console.log("Downloading " + file_url)
+				var file_name = querystring.unescape(file_url.split('/')[file_url.split('/').length - 1])
+				if (opts.debug) console.log("File name is :" + file_name)
+				request(file_url).pipe(fs.createWriteStream(file_name))
+				console.log("File saved.")
+			}
 		}
-	}
 
-});
+	});
+}
+download();
+setInterval(download, time * 1000);
